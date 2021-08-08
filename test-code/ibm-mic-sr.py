eb38d6e151d5b4774b1804820a8a1983a614a600
yosuakurniawan <yosua.kurniawan@studmail.w-hs.de>
2021-08-08 22:25:33 +0200

    add source code for ibm watson

---
+++ test-code/ibm-mic-sr.py
@@ -0,0 +1,28 @@
+from ibm_watson import SpeechToTextV1
+from ibm_watson.websocket import RecognizeCallback, AudioSource 
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import speech_recognition as sr
+apikey = 'bf5PikK1pKAzKWWBVKSIcJTRKCZZ1Ty-Mizqsnog2-i4'
+url = 'https://api.eu-de.speech-to-text.watson.cloud.ibm.com/instances/c94da279-2d44-4c49-97ba-1c958331973e'
+
+authenticator = IAMAuthenticator(apikey)
+stt = SpeechToTextV1(authenticator=authenticator)
+stt.set_service_url(url)
+
+print("Authentication Sucsess")
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak Anything :")
+    res = stt.recognize(audio=source, content_type='audio/wav', model='en-US_NarrowbandModel', continuous=True).get_result()
+    
+text = res['results'][0]['alternatives'][0]['transcript']
+print(text)
+
+    # audioin = r.listen(source)
+    # try:
+    #     res = stt.recognize(audio=audioin, content_type='audio/wav', model='en-US_NarrowbandModel', continuous=True).get_result()
+    #     text = res['results'][0]['alternatives'][0]['transcript']
+    #     print(text)
+    # except:
+    #     print("Sorry could not recognize what you said")
--- test-code/test-speech-recognition.py
+++ test-code/test-speech-recognition.py
@@ -9,12 +9,30 @@
 
 import speech_recognition as sr
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Speak Anything :")
-    audio = r.listen(source)
-    try:
-        text = r.recognize_google(audio)
-        print("You said : {}".format(text))
-    except:
-        print("Sorry could not recognize what you said")
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak Anything :")
+        audio = r.listen(source)
+        text = ""
+        try:
+            text = r.recognize_google(audio)
+            print(text)
+        except:
+            print("Unrecognizeable")
+    return text
+
+text_lst = get_audio()
+
+if "forward" in text_lst:
+    print("Moving Forward")
+elif "backward" in text_lst:
+    print("Moving Backward")
+elif "left" in text_lst:
+    print("Turning Left")
+elif "right" in text_lst:
+    print("Turning Right")    
+elif "stop" in text_lst:
+    print("Stop Movement") 
+else:
+    print("UNABLE TO RECOGNIZE COMMAND") 
